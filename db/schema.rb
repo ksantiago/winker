ad6d30dfea171328c296c08fbde6611491c5ac66
Katherine Santiago <santiago.kat@gmail.com>
2013-12-06 16:32:22 -0500

    merge issues

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20131206195903) do
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "sex"
+    t.string   "img_url"
+    t.string   "email"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
